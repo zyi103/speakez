e3da27ec2b644c8985352330df059eef1afcde30
zijunyi <zyi103@syr.edu>
2019-06-24 16:58:37 +0800

    start memcached server

--- speakez/speakez_core/entrypoint.sh
+++ speakez/speakez_core/entrypoint.sh
@@ -5,3 +5,6 @@ python /var/webapp/manage.py migrate
 
 echo "Collecting Static Assets"
 python /var/webapp/manage.py collectstatic --noinput
+
+echo "starting memcached server"
+service memcached start
