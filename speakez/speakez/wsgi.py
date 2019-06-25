b30509556c9ee5a48bd5f1490f78a85523050108
zijunyi <zyi103@syr.edu>
2019-06-25 10:47:04 +0800

    ptvsd

--- speakez/speakez/wsgi.py
+++ speakez/speakez/wsgi.py
@@ -14,3 +14,11 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'speakez.settings')
 
 application = get_wsgi_application()
+
+
+import ptvsd
+
+try:
+    ptvsd.enable_attach("my_secret", address=('localhost', 3000))
+except:
+    pass
